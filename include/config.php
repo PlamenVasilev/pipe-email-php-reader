450d203b594391d0cb343cb407eeaa87f8080c0d
Ivan Ivanov <iki03@mail.bg>
2015-09-27 23:19:54 +0300

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,67 +1,45 @@
 <?php
 
 
-
 /* 
-
  * @desc configuration
-
  */
 
-
-
 /** 
-
  * What's the max # of seconds to try to process
-
  */
 
 define('MAX_TIME_LIMIT', 600);
 
 
-
 /**
-
  * Configure your MySQL database connection here
-
  */
 
 
-
 /** 
-
  * MySQL hostname
-
  */
 
 define('DB_HOST', 'localhost');
 
 
-
 /**
-
  * The name of the database for WordPress
-
  */
 
 define('DB_NAME', '******');
 
 
-
 /**
-
  * MySQL database username
-
  */
 
 define('DB_USER', '******');
 
 
-
 /**
-
  * MySQL database password
-
  */
 
 define('DB_PASSWORD', '******');
